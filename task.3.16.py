b8e4a47ddd507b7ba6fbee0a9cb58ac0729c98a4
Umka6 <67368769+Umka6@users.noreply.github.com>
2020-08-15 15:31:12 +0600

    Add files via upload

---
+++ task.3.16.py
@@ -0,0 +1,18 @@
+a = int(input("enter the number"))
+b = int(input('enter the number'))
+c = int(input("enter the number"))
+if a > 10:
+    print('yes')
+    if b > 10:
+        print("yes")
+    if c > 10:
+        print("yes")
+
+elif a < 10:
+    print("no")
+elif b < 10:
+    print("no")
+elif c < 10:
+    print("no")
+
+
